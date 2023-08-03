aab35773a52ef0d2917d7b488978d4cdb65de364
novesh <gautamnovesh@gmail.com>
2023-08-03 14:41:01 +0530

    loginpage

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,170 +1,344 @@
-import React,{useState} from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import React, { useState } from "react";
 import image from "./images/Frame.jpg.png";
-import MyForm from './Form';
+import frame2image from "./images/frame2.jpg.png";
 
 const LoginForm = () => {
-  const initialValues = {
-    username: "",
-    password: "",
+
+  const [isSignInClicked, setIsSignInClicked] = useState(false);
+
+  const handleSignInClick = () => {
+    setIsSignInClicked(true);
   };
 
-  const onSubmit = (values) => {
-   
-    console.log(values);
-  };
+  if (!isSignInClicked) {
 
-  const validate = (values) => {
-    const errors = {};
-
-    if (!values.username) {
-      errors.username = "username Required";
-    }
-
-    if (!values.password) {
-      errors.password = "password Required";
-    }
-
-    return errors;
-  };
-
-  return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
-      <Form>
-        <div>
-          <label htmlFor="username" style={styles.label}>Username</label>
-          <Field type="username" id="username" name="username" style={styles.inputField} />
-          <ErrorMessage name="username" component="div"  style={styles.errorText}/>
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            flex: 0.5,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{ width: "350px", height: "300px", position: "relative" }}
+          >
+            <img
+              src={frame2image}
+              style={{ width: "350px", height: "40px", background: "white" }}
+              alt="Image"
+            />
+            <div
+              style={{
+                width: "375px",
+                height: "50px",
+                background: "white",
+                position: "absolute",
+                top: "30%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <h2
+                style={{
+                  fontFamily: "Rubik",
+                  fontWeight: "350",
+                  fontSize: "24px",
+                  color: " #093A5E",
+                }}
+              >
+                Hi, Welcome Back
+              </h2>
+            </div>
+            <div
+              style={{
+                width: "350px",
+                height: "40px",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <button
+                style={{
+                  background: "#004679",
+                  color: "white",
+                  width: "350px",
+                  height: "40px",
+                  border: "none",
+                  cursor: "pointer",
+                  fontSize: "18px",
+                }}
+                onClick={handleSignInClick}
+              >
+                Sign In
+              </button>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                width: "350px",
+                height: "70px",
+                bottom: "0px",
+                left: "50%",
+                transform: "translateX(-50%)",
+                color: "black",
+                background: "white",
+              }}
+            >
+              <div
+                style={{
+                  width: "350px",
+                  height: "35px",
+                  textAlign: "center",
+                  color: "gray",
+                }}
+              >
+                don't have an account?
+              </div>
+              <div
+                style={{
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                  width: "350px",
+                  height: "35px",
+                  textAlign: "center",
+                  color: "blue",
+                }}
+              >
+                signup
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password" style={styles.label}>Password</label>
-          <Field type="password" id="password" name="password" style={styles.inputField}/>
-          <ErrorMessage name="password" component="div"  style={styles.errorText}/>
+        <div style={{ flex: 0.5, display: "flex" }}>
+          <img src={image} style={{ width: "832px" }} alt="Image" />
         </div>
-        <button type="submit" style={styles.loginButton}>Log In</button>
-        <hr className="divider" />
-       
-      </Form>
-    </Formik>
-  );
-};
-
-const App = () => {
-  const [Signup, setSignup] = useState(false);
-
-  const SignupClick = () => {
-    setSignup(true);
-  };
-
-  const LoginClick = () => {
-    setSignup(false);
-  };
-
-  if (Signup) {
-    return <MyForm />;
-  }
-
-  return (
-    <div style={styles.containerStyle}>
-      <div style={styles.subcontainer1Style}>
-        <div style={styles.miniContainer}>
-       
+      </div>
+    );
+  } else {
+    
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "100vh",
+          
+        }}
+      >
+        <div
+          style={{
+            flex: 0.5,
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            position:'relative'
+           
+          }}
+        >
+           <img
+            src={frame2image}
+            style={{
+              width: "350px",
+              height: "40px",
+              background: "white",
+              marginTop: "-10px",
+              
+            }}
+            alt="Image"
+          />
          
-          <LoginForm />
+          <div
+            style={{
+              position: "relative",
+              width: "550px",
+              height: "650px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            
+            }}
+          >
+            <div
+              style={{ width: "310px", height: "550px", position: "relative" }}
+            >
+              <h2
+                style={{
+                  marginTop: "0px",
+                  fontFamily: "Rubik",
+                  fontWeight: "310",
+                  fontSize: "24px",
+                  color: "#093A5E",
+                  width: "310px",
+                  height: "30px",
+                  textAlign: "center",
+                }}
+              >
+                Sign In
+              </h2>
+              <p
+                style={{
+                  width: "310px",
+                  height: "30px",
+                  textAlign: "center",
+                  color: "gray",
+                }}
+              >
+                Enter your credentials to continue
+              </p>
 
-          <button type="button" style={styles.signUpButton} onClick={SignupClick}>signup</button>
+              <div
+                style={{
+                  width: "310px",
+                  height: "220px",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <input
+                  type="email"
+                  style={{
+                    width: "300px",
+                    height: "40px",
+                    padding: "5px",
+                    marginBottom: "10px",
+                    fontSize: "16px",
+                    borderRadius: "5px",
+                    border: "1px solid #ccc",
+                    background: "rgba(250,250,250,255)",
+                  }}
+                  placeholder="Email Address"
+                />
+                <input
+                  type="password"
+                  style={{
+                    width: "300px",
+                    height: "40px",
+                    padding: "5px",
+                    marginBottom: "10px",
+                    fontSize: "16px",
+                    borderRadius: "5px",
+                    border: "1px solid #ccc",
+                    background: "rgba(250,250,250,255)",
+                  }}
+                  placeholder="Password"
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <label
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: "#212121",
+                      fontFamily: "roboto",
+                      fontSize: "18px",
+                      fontWeight: "150",
+                      
+                    }}
+                  >
+                    <input type="checkbox" />
+                    Remember me
+                  </label>
+                  <div
+                    style={{
+                      textDecoration: "underline",
+                      color: "#2E1760",
+                      textAlign: "center",
+                      
+                    }}
+                  >
+                    Forgot Password?
+                  </div>
+                </div>
+                <button
+                  style={{
+                    background: "#004679",
+                    color: "white",
+                    width: "100%",
+                    height: "40px",
+                    border: "none",
+                    cursor: "pointer",
+                    fontSize: "18px",
+                    borderRadius: "5px",
+                    marginTop:'30px'
+                  }}
+                >
+                  Sign In
+                </button>
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  width: "350px",
+                  height: "70px",
+                  bottom: "0px",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  color: "black",
+                  background: "white",
+                }}
+              >
+                <div
+                  style={{
+                    width: "350px",
+                    height: "35px",
+                    textAlign: "center",
+                    color: "gray",
+                  }}
+                >
+                  don't have an account?
+                </div>
+                <div
+                  style={{
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                    width: "350px",
+                    height: "35px",
+                    textAlign: "center",
+                    color: "blue",
+                  }}
+                >
+                  signup
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{ flex: 0.5, display: "flex" }}>
+          <img src={image} style={{ width: "832px" }} alt="Image" />
         </div>
       </div>
-      <div style={styles.subcontainer2Style}>
-        <img src={image} style={styles.image} alt="Image" />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
-const styles = {
-  containerStyle: {
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    height: "100vh",
-    background: "black",
-  },
-
-  subcontainer1Style: {
-    flex: 0.5,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-   
-    background: "white",
-  },
-
-  subcontainer2Style: {
-    flex: 0.5,
-    display: "flex",
-
-   
-  },
-  image: {
-    width: "832px",
-  },
-  
-  miniContainer: {
-    height: "550px",
-    width: "500px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-   // background: "lightblue",
-    borderRadius: "10px",
-  },
-  loginButton: {
-    backgroundColor: "blue",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    margin: "20px 0",
-    border: "none",
-    cursor: "pointer",
-    width: '100px',
-    height: '40px'
-  },
-
-  signUpButton: {
-    backgroundColor: "green",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    margin: "10px 0",
-    border: "none",
-    cursor: "pointer",
-    width: '100px',
-    height: '40px'
-  },
-  inputField: {
-    width: "90%",
-    padding: "10px",
-    fontSize: "16px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-    margin: "2px 5",
-  },
-
-  label: {
-    display: "block",
-    marginBottom: "0px",
-    fontSize: "20px",
-    fontWeight: '100',
-    marginTop: '15px'
-  },
-  errorText: {
-    color: "red",
-    fontSize: "14px",
-    marginTop: "5px",
-  },
-};
-
-export default App;
+export default LoginForm;
